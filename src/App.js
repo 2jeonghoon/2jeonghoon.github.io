dacdcf0f0612fdd89eb93bfe71f6c983fc268567
2jeonghoon <2jh0926@naver.com>
2024-04-03 15:14:11 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomeView from "./views/HomeView";
 
 const App = () => {
-    return <div>App</div>;
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomeView />} />
+        </Routes>
+    </BrowserRouter>
+    );
 };
 
 export default App;
+
+                        
+                        
+                            
+                                
+출처: https://webstoryboy.co.kr/1935 [WEBSTORYBOY:티스토리]
