d9c5a2ff5bc555ca4eebc26d1488dd6c72b5f382
2jeonghoon <2jh0926@naver.com>
2024-04-03 15:30:56 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import React, { useEffect } from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomeView from "./views/HomeView";
+import smooth from "./utils/smooth";
+import link from "./utils/link";
 
 const App = () => {
+    useEffect(() => {
+        smooth();
+        link();
+    }, []);
+
     return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomeView />} />
-        </Routes>
-    </BrowserRouter>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomeView />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
 export default App;
-
-                        
-                        
-                            
-                                
-출처: https://webstoryboy.co.kr/1935 [WEBSTORYBOY:티스토리]
